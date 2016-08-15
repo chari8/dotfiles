23dd42d6a8b5faa8044dd004af23c4b88995eb5d
chari8 <ghoti3.14@gmail.com>
2016-08-15 15:03:05 +0900

    adjustment

--- setup_apt.sh
+++ setup_apt.sh
@@ -19,7 +19,7 @@ echo "install vim" >> $PWD/log.txt
 mkdir "$PWD/src" >> $PWD/log.txt
 (cd src;
 wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2;
-tar xjfC vim-7.4.tar.bz2;
+tar xjf vim-7.4.tar.bz2;
 cd vim74;
 ./configure \
 --with-features=huge \
