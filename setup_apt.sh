a07aedf36e0d669b42e15f3f516eba00832f954d
chari8 <ghoti3.14@gmail.com>
2016-08-15 14:26:30 +0900

    use lua

--- setup_apt.sh
+++ setup_apt.sh
@@ -1,6 +1,37 @@
 #!/bin/bash
+echo "*****************************************\n" >> $PWD/log.txt 
 date >> $PWD/log.txt
-apt install vim >> $PWD/log.txt
+echo "remove old vim" >> $PWD/log.txt 
+apt remove vim >> $PWD/log.txt
+echo "install mercurial" >> $PWD/log.txt 
+apt install mercurial >> $PWD/log.txt
+echo "install ncurses-devel" >> $PWD/log.txt 
+apt install ncurses-devel >> $PWD/log.txt
+echo "install lua & luajit" >> $PWD/log.txt 
+apt install lua >> $PWD/log.txt
+apt install lua-devel >> $PWD/log.txt
+apt install luajit >> $PWD/log.txt
+echo "install python & python3" >> $PWD/log.txt 
+apt install python >> $PWD/log.txt
+apt install python3-devel >> $PWD/log.txt
+
+echo "install vim" >> $PWD/log.txt 
+wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2 >> $PWD/log.txt
+tar xjfC vim-7.4.tar.bz2 $PWD/src >> $PWD/log.txt
+(cd src/vim74;
+./configure \
+--with-features=huge \
+--enable-multibyte \
+--enable-luainterp=dynamic \
+--enable-gpm \
+--enable-cscope \
+--enable-fontset \
+--enable-fail-if-missing \
+--prefix=/usr/local;
+make;
+make install;) >> $PWD/log.txt
+
+echo "set vimrc & dein" >> $PWD/log.txt 
 mkdir -p "$HOME/.vim/rc" >> $PWD/log.txt 
 mkdir -p "$HOME/.vim/color" >> $PWD/log.txt 
 mkdir -p "$HOME/.vim/undo" >> $PWD/log.txt 
