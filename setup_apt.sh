74c2b71aa79e8089d98271eccc85a5362e96fa7c
chari8 <ghoti3.14@gmail.com>
2016-08-15 14:58:33 +0900

    adjust

--- setup_apt.sh
+++ setup_apt.sh
@@ -16,9 +16,11 @@ apt install python >> $PWD/log.txt
 apt install python3-devel >> $PWD/log.txt
 
 echo "install vim" >> $PWD/log.txt 
-wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2 >> $PWD/log.txt
-tar xjfC vim-7.4.tar.bz2 $PWD/src >> $PWD/log.txt
-(cd src/vim74;
+mkdir "$PWD/src" >> $PWD/log.txt
+(cd src;
+wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2;
+tar xjfC vim-7.4.tar.bz2;
+cd vim74;
 ./configure \
 --with-features=huge \
 --enable-multibyte \
@@ -35,7 +37,7 @@ echo "set vimrc & dein" >> $PWD/log.txt
 mkdir -p "$HOME/.vim/rc" >> $PWD/log.txt 
 mkdir -p "$HOME/.vim/color" >> $PWD/log.txt 
 mkdir -p "$HOME/.vim/undo" >> $PWD/log.txt 
-echo "creating symbolic links\n" >> $PWD/log.txt 
+echo "creating symbolic links" >> $PWD/log.txt 
 ln -s -f "$PWD/.vimrc" "$HOME/.vim/vimrc" >> $PWD/log.txt  
 ln -s -f "$PWD/dein.toml" "$HOME/.vim/rc/dein.toml" >> $PWD/log.txt 
 ln -s -f "$PWD/dein_lazy.toml" "$HOME/.vim/rc/dein_lazy.toml" >> $PWD/log.txt 
--- setup_yum.sh
+++ setup_yum.sh
@@ -15,10 +15,13 @@ echo "install python & python3" >> $PWD/log.txt
 yum install python >> $PWD/log.txt
 yum install python3-devel >> $PWD/log.txt
 
+
 echo "install vim" >> $PWD/log.txt 
-wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2 >> $PWD/log.txt
-tar xjfC vim-7.4.tar.bz2 $PWD/src >> $PWD/log.txt
-(cd src/vim74;
+mkdir "$PWD/src" >> $PWD/log.txt
+(cd src;
+wget ftp://ftp.vim.org/pub/vim/unix/vim-7.4.tar.bz2;
+tar xjfC vim-7.4.tar.bz2;
+cd vim74;
 ./configure \
 --with-features=huge \
 --enable-multibyte \
@@ -35,7 +38,7 @@ echo "set vimrc & dein" >> $PWD/log.txt
 mkdir -p "$HOME/.vim/rc" >> $PWD/log.txt 
 mkdir -p "$HOME/.vim/color" >> $PWD/log.txt 
 mkdir -p "$HOME/.vim/undo" >> $PWD/log.txt 
-echo "creating symbolic links\n" >> $PWD/log.txt 
+echo "creating symbolic links" >> $PWD/log.txt 
 ln -s -f "$PWD/.vimrc" "$HOME/.vim/vimrc" >> $PWD/log.txt  
 ln -s -f "$PWD/dein.toml" "$HOME/.vim/rc/dein.toml" >> $PWD/log.txt 
 ln -s -f "$PWD/dein_lazy.toml" "$HOME/.vim/rc/dein_lazy.toml" >> $PWD/log.txt 
