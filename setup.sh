de382504d8c1e06d4450c29f7cd2eecc071acc11
chari8 <ghoti3.14@gmail.com>
2018-06-11 10:16:14 +0900

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-# apt install vim-nox
-mkdir -p "$HOME/.vim/rc"
-ln -s "$PWD/.vimrc" "$HOME/.vim/vimrc"  
-ln -s "$PWD/dein.toml" "$HOME/.vim/rc/dein.toml"
-ln -s "$PWD/dein_lazy.toml" "$HOME/.vim/rc/dein_lazy.toml"
+VIMDIR="$HOME/.vim"
+RCDIR="$HOME/.vim/rc"
+if [ ! -d $VIMDIR ]; then
+	mkdir $VIMDIR
+fi
+
+if [ ! -d $RCDIR ]; then
+	mkdir $RCDIR
+fi
+
+ln -is "$PWD/.vimrc" "$HOME/.vim/vimrc"  
+ln -is "$PWD/dein.toml" "$HOME/.vim/rc/dein.toml"
+ln -is "$PWD/dein_lazy.toml" "$HOME/.vim/rc/dein_lazy.toml"
